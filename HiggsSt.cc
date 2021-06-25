732c9365744db842796d3d8f59efb8ec39f55a5d
pradyot pritam <42238958+pradyotpsahoo@users.noreply.github.com>
2021-06-25 18:04:48 +0530

    Add files via upload

--- HiggsSt.cc
+++ HiggsSt.cc
@@ -5,46 +5,52 @@
 #include "TLorentzVector.h"
 #include "TMath.h"
 
-
 using namespace Pythia8;
-int main()
- {
-  Pythia pythia;
-  pythia.readString("Beams:idA =  11");  
-  pythia.readString("Beams:idB = -11");
-  pythia.readString("Beams:eCM = 500.0");  
-  pythia.readString(" HiggsSM:ffbar2HZ = on"); // Higgs-Stralung process
+
+int main(){ 
+Pythia pythia;
+pythia.readString("Beams:idA =  11");  
+pythia.readString("Beams:idB = -11");
+pythia.readString("Beams:eCM = 500.0");  
+pythia.readString(" HiggsSM:ffbar2HZ = on"); 
+// Higgs-Stralung process
   
-  pythia.init();
+pythia.init();
   
-  TFile * out_file = new TFile("output_HiggsSt.root","RECREATE");
-  
-  
-  //Angle between mu+ and mu- , e+ and e- in Z decay  
-  TH1F * hist1d_angle_mumu = new TH1F("hist1d_Angle_mumu", "Z0 #rightarrow #mu_{+} + #mu_{-}", 100, 0., TMath::Pi());
-  TH1F * hist1d_angle_ee = new TH1F("hist1d_Angle_ee", "Z0 #rightarrow e_{+} + e_{-}", 100, 0., TMath::Pi());
-  
-  
-  //Mass invarient in  Z decay 
-  TH1F * hist1d_mass_mumu = new TH1F("hist1d_mass_Zmumu", "Z0 #rightarrow #mu_{+} + #mu_{-}", 100, 50., 150.);
-  TH1F * hist1d_mass_ee = new TH1F("hist1d_mass_Zee", "Z0 #rightarrow e_{+} + e_{-}", 100, 50., 150.);
+TFile * out_file = new TFile("output_HiggsSt.root","RECREATE");
+   
+//Angle between mu+ and mu- , e+ and e- in Z decay  
+TH1F * hist1d_angle_mumu = new TH1F("hist1d_Angle_mumu", "Z0 #rightarrow #mu_{+} + #mu_{-}", 100, 0., TMath::Pi());
+TH1F * hist1d_angle_ee = new TH1F("hist1d_Angle_ee", "Z0 #rightarrow e_{+} + e_{-}", 100, 0., TMath::Pi());
   
   
-  //Angle between Z and H in e- + e+ -> H + Z process 
-  TH1F * hist1d_angle_ZH = new TH1F("hist1d_Angle_ZH", "e_{+} + e_{-} #rightarrow Z0 + H ", 300, 2.6 , 3.4);
+//Mass invarient in  Z decay 
+TH1F * hist1d_mass_mumu = new TH1F("hist1d_mass_Zmumu", "Z0 #rightarrow #mu_{+} + #mu_{-}", 100, 50., 150.);
+TH1F * hist1d_mass_ee = new TH1F("hist1d_mass_Zee", "Z0 #rightarrow e_{+} + e_{-}", 100, 50., 150.);
+  
+  
+//Angle between Z and H in e- + e+ -> H + Z process 
+TH1F * hist1d_angle_ZH = new TH1F("hist1d_Angle_ZH", "e_{+} + e_{-} #rightarrow Z0 + H ", 300, 2.6 , 3.4);
+TH1F * hist1d_pT_ZH = new TH1F("hist1d_pT_ZH", "e_{+} + e_{-} #rightarrow Z0 + H ", 300, -100, 100);
 
 
-  for (int iEvent = 0; iEvent < 20000; ++iEvent) // 20000 event generated
-  {
-    if (!pythia.next()) continue;
+for (int iEvent = 0; iEvent < 20000; ++iEvent) // 20000 event generated
+{
+	
+	if (!pythia.next()) continue;
+	
     for (int i = 0; i < pythia.event.size(); ++i)
-    {	
+    
+    {
+    	
 	if(pythia.event[i].id() == 23) 
     {
-    	int dau1 = pythia.event[i].daughter1();
-    	int dau2 = pythia.event[i].daughter2();
-    	if (pythia.event[dau1].id() == 13 and pythia.event[dau2].id() == -13 ) 
-    	// Sort out the process : Z0--> mu+ + mu-
+    	
+		int dau1 = pythia.event[i].daughter1();
+		int dau2 = pythia.event[i].daughter2();
+    	
+		if (pythia.event[dau1].id() == 13 and pythia.event[dau2].id() == -13 ) 
+		// Sort out the process : Z0-> mu+ + mu-
     	{	
     		int j = i;
     		while(j>0)
@@ -53,8 +59,9 @@ int main()
 
     			if (pythia.event[mot1].id() != 23)
     			//actual parent 
+    			
     			{	
-
+					
     				//cout << pythia.event[j].name()<<"->" <<pythia.event[dau1].name() << " + " << pythia.event[dau2].name() << endl;		
     				TLorentzVector pmu1;
     				double pt_mu1 = pythia.event[dau1].pT();
@@ -71,15 +78,15 @@ int main()
     				pmu2.SetPtEtaPhiM(pt_mu2, eta_mu2, phi_mu2, m_mu2);
     				
     				double angle = pmu1.Angle(pmu2.Vect()); //Angle between mu+ and mu- 
-				double mass = (pmu1 + pmu2).M(); // Invarient mass calculation 
+					double mass = (pmu1 + pmu2).M(); // Invarient mass calculation 
 				
-				hist1d_angle_mumu -> Fill(angle); 
-				hist1d_mass_mumu -> Fill(mass);
+					hist1d_angle_mumu -> Fill(angle); 
+					hist1d_mass_mumu -> Fill(mass);
     				break;
     			}
     			else
     			{	 				
- 				j = mot1;	
+ 					j = mot1;	
     			}
     		}
     	
@@ -87,15 +94,18 @@ int main()
     	
     	else if(pythia.event[dau1].id() == 11) 
     	// Sort out the process : Z0--> e+ + e-
-    		{
+    	{
+    		
     		int j = i;
+    		
     		while(j>0)
     		{	
     			int mot1 = pythia.event[j].mother1();
+    			
     			if (pythia.event[mot1].id() != 23)
     			{	
     				//cout << pythia.event[j].name()<<"->" <<pythia.event[dau1].name() << " + " << pythia.event[dau2].name() << endl;
-				TLorentzVector pe1;
+					TLorentzVector pe1;
     				double pt_e1 = pythia.event[dau1].pT();
     				double eta_e1 = pythia.event[dau1].eta();
     				double phi_e1 = pythia.event[dau1].phi();
@@ -110,28 +120,34 @@ int main()
     				pe2.SetPtEtaPhiM(pt_e2, eta_e2, phi_e2, m_e2);    			
     				
     				double angle = pe1.Angle(pe2.Vect()); //Angle between e+ and e- 
-				double mass = (pe1 + pe2).M(); // invarient mass calculation
+					double mass = (pe1 + pe2).M(); // invarient mass calculation
 				
     				hist1d_angle_ee -> Fill(angle);
     				hist1d_mass_ee -> Fill(mass);
     				break;
     			}
+    			
     			else
     			{	 				
- 				j = mot1;	
+ 					j = mot1;	
     			}
     			
-    			}
     		}
+    		
+    	}
     }
     else if(pythia.event[i].id() == 11) 
     {
+    	
     	int dau1 = pythia.event[i].daughter1();
     	int dau2 = pythia.event[i].daughter2();
+    	
     	if (pythia.event[dau1].id() == 25 and pythia.event[dau2].id() == 23)
     	//Sort out the process:  e- + e+ -> Z + H 
+		
+		
 		{
-    				//cout <<"->" <<pythia.event[dau1].name() << " + " << pythia.event[dau2].name() << endl;
+					//cout <<"->" <<pythia.event[dau1].name() << " + " << pythia.event[dau2].name() << endl;
     				TLorentzVector pH;
     				double pt_pH = pythia.event[dau1].pT();
     				double eta_pH = pythia.event[dau1].eta();
@@ -148,16 +164,22 @@ int main()
     				
     				double angle = pH.Angle(pZ.Vect()); // Angle between H and Zboson
     				hist1d_angle_ZH -> Fill(angle);
+    				hist1d_pT_ZH -> Fill(pt_pH - pt_pZ);
 		}
-	else continue;	
-    	}
+	
+		else continue;	
+    }
     
 	else continue;
+	
 	}
-  }
+
+}
   
-  pythia.stat();
-  out_file->Write();
-  out_file->Close();
-  return 0;
-  }
+//pythia.stat();
+out_file->Write();
+out_file->Close();
+return 0;
+}
+
+
